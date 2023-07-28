2369684f71c7cedfbcf3d3ad28dc197cfabe3951
VictorCstr <victorcastro.dev@hotmail.com>
2023-07-28 10:58:48 -0300

    feat: balanceamento de carga no server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import compression from "compression";
 import routes from "./rotas/RotasPedidos";
 
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(compression());
 app.use(cors());
 app.use(routes);
 
--- src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from "./app";
-const port = process.env.PORT || 9090;
-
-app.listen(port, () => {
-  console.log(`Aplicação rodando na porta: ${port}`);
-});
